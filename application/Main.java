2ab55077ca18147e31663d74eaecf8740deffb44
Rafael Rocha <rafaelrocha250304@gmail.com>
2026-04-30 18:47:34 -0300

    exerc 1

--- application/Main.java
+++ application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
-import entities.Account;
+import entities.Livro;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,46 +9,13 @@ public class Main {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Account user;
-        String response;
-        double sacado;
 
+        Livro livro1, livro2;
+        livro1 = new Livro();
+        livro2 = new Livro("Sapiens", "Rafael Rocha", 438);
 
-        System.out.print("Enter account number: ");
-        int numConta = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter account holder: ");
-        String nome = sc.nextLine();
-        System.out.print("Is there na initial deposit (y/n)? ");
-        response = sc.nextLine();
-        System.out.println(" ");
-
-        if (response.equals("y")){
-            System.out.print("Enter initial deposit value: ");
-            double initialDeposit = sc.nextDouble();
-            user = new Account(numConta, nome, initialDeposit);
-        }else{
-            user = new Account(numConta, nome);
-        }
-
-        System.out.println("Account data: ");
-        System.out.println(user);
-
-        System.out.print("Enter a deposit value: ");
-        double depositValue = sc.nextDouble();
-        user.depositar(depositValue);
-
-        System.out.println(" ");
-        System.out.println("Updated account data:");
-        System.out.println(user);
-
-        System.out.println(" ");
-        System.out.print("Enter a withdraw value: ");
-        double withdrawValue = sc.nextDouble();
-        user.sacar(withdrawValue);
-
-        System.out.println(user);
-
+        System.out.println(livro1);
+        System.out.println(livro2);
 
         sc.close();
 
