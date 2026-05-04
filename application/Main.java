675ae7413da162e36905de1b87fc3a4288389d6a
Rafael Rocha <rafaelrocha250304@gmail.com>
2026-05-04 18:21:23 -0300

    exercicio 8 vetores

--- application/Main.java
+++ application/Main.java
@@ -8,36 +8,42 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int n;
-        double soma, media;
+        int n, qntdPares;
+        double soma, media, resto;
+
+
+        resto = 0;
+        qntdPares = 0;
+        soma= 0;
+        media = 0;
 
         System.out.print("Quantos elementos vai ter cada vetor? ");
         n = sc.nextInt();
         double[] vetor = new double[n];
 
-        for (int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){
             System.out.print("Digite um numero: ");
             vetor[i] = sc.nextDouble();
         }
 
-        soma = 0;
-        for (int i = 0; i < n; i++){
-            soma += vetor[i];
-        }
 
-        media = soma / n;
-
-        System.out.println("MEIDA DO VETOR = " + media);
-
-        System.out.println("ELEMENTOS ABAIXO DA MEDIA:");
 
         for (int i = 0; i < n; i++){
-            if(vetor[i] < media){
-                System.out.println(vetor[i]);
+            resto = vetor[i] % 2;
+
+            if(resto == 0){
+                qntdPares++;
+                soma += vetor[i];
             }
         }
 
+        media = soma / qntdPares;
 
+        if(qntdPares == 0){
+            System.out.println("NENHUM NUMERO PAR");
+        }else{
+            System.out.println("MEDIA DOS PARES = " + media);
+        }
 
 
 
