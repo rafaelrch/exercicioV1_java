aff582ea25ec776d6fb9e9ea1585e3125273e20c
Rafael Rocha <rafaelrocha250304@gmail.com>
2026-05-05 15:00:45 -0300

    exercicio employee

--- application/Main.java
+++ application/Main.java
@@ -1,43 +1,57 @@
 package application;
 
-import entities.Rooms;
+import entities.Employee;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         int n;
 
-        System.out.print("HOW MANY ROOMS WILL BE RENTED? ");
+        System.out.print("How many employees will be registered? ");
         n = sc.nextInt();
-        Rooms[] rooms = new Rooms[10];
 
+        List<Employee> employeeList = new ArrayList<>();
 
-
-        for(int i = 0; i < n; i++){
-            System.out.println("Rent #" + (i+1) + ":");
-            System.out.print("Name: ");
+        for (int i = 0; i < n; i++){
+            System.out.println("Employee #" + (i+1) + ":");
+            System.out.print("Id: ");
+            int id = sc.nextInt();
             sc.nextLine();
+            System.out.print("Name: ");
             String name = sc.nextLine();
-            System.out.print("Email: " );
-            String email = sc.nextLine();
-            System.out.print("Room: ");
-            int room = sc.nextInt();
+            System.out.print("Salary: ");
+            double salary = sc.nextDouble();
 
-            Rooms quarto = new Rooms(name, email, room);
-            rooms[room] = quarto;
+            Employee employee = new Employee(id, name, salary);
+            employeeList.add(employee);
+
         }
 
-        System.out.println("Busy rooms: ");
-        for (int i = 0; i < rooms.length; i++){
-            if (rooms[i] != null){
-                System.out.println(rooms[i]);
+        System.out.print("Enter the employee id that will have salary increase: ");
+        int idIncrease = sc.nextInt();
+
+        List<Employee> result = employeeList.stream().filter(x -> x.getId() == idIncrease).collect(Collectors.toList());
+
+        if(result.isEmpty()){
+            System.out.println("This id does not exist!");
+        }else{
+            System.out.println("Enter the percentage: ");
+            double percentage = sc.nextDouble();
+            for(Employee x : result){
+                x.setSalary(x.getSalary() + x.getSalary() * percentage / 100);
             }
         }
+
+        System.out.println("List of employees:");
+        System.out.println(employeeList);
+
 
         sc.close();
 
