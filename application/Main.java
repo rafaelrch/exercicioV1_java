08c369efc6696d8a4eba29183de614732ab4191b
Rafael Rocha <rafaelrocha250304@gmail.com>
2026-05-16 20:41:22 -0300

    exemplo 2 de resolucao

--- application/Main.java
+++ application/Main.java
@@ -11,31 +11,35 @@ public class Main {
     public static void main(String[] args) throws ParseException{
         Scanner sc = new Scanner(System.in);
 
-       Set<Course> set = new HashSet<>();
+
+       Set<Integer> a = new HashSet<>();
+       Set<Integer> b = new HashSet<>();
+       Set<Integer> c = new HashSet<>();
 
         System.out.print("How many students for course A? ");
         int nStudents = sc.nextInt();
         for (int i = 0; i < nStudents; i++){
             int id = sc.nextInt();
-
-            set.add(new Course(id));
+            a.add(id);
         }
 
         System.out.print("How many students for course B? ");
         int nStudents2 = sc.nextInt();
         for (int i = 0; i < nStudents2; i++){
             int id = sc.nextInt();
-
-            set.add(new Course(id));
+            b.add(id);
         }
 
         System.out.print("How many students for course C? ");
         int nStudents3 = sc.nextInt();
         for (int i = 0; i < nStudents3; i++){
             int id = sc.nextInt();
-
-            set.add(new Course(id));
+            c.add(id);
         }
+
+        Set<Integer> set = new HashSet<>(a);
+        set.addAll(b);
+        set.addAll(c);
 
         System.out.println("Total students: " + set.size());
         sc.close();
