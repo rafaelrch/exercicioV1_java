0b8f92e8917dc4d416cd87c22a5667bc9f857aa4
Rafael Rocha <rafaelrocha250304@gmail.com>
2026-05-16 20:39:39 -0300

    exemplo 1 de resolucao

--- application/Main.java
+++ application/Main.java
@@ -1,13 +1,9 @@
 package application;
 
-import entities.BankSlip;
-import entities.CreditCard;
-import entities.Order;
-import entities.Pix;
+import entities.Course;
 
 import java.text.ParseException;
 
-import java.time.LocalDate;
 import java.util.*;
 
 public class Main {
@@ -15,26 +11,33 @@ public class Main {
     public static void main(String[] args) throws ParseException{
         Scanner sc = new Scanner(System.in);
 
-        List<Order> list = new ArrayList<>();
+       Set<Course> set = new HashSet<>();
 
-        Order order1 = new Order(1111, 1000.0, new CreditCard(9999));
-        Order order2 = new Order(2222, 2000.0, new Pix("rafael@gmail.com"));
-        Order order3 = new Order(3333, 3000.0, new BankSlip(LocalDate.of(2024, 12, 20)));
-        Order order4 = new Order(3333, 4000.0, new CreditCard(8888));
+        System.out.print("How many students for course A? ");
+        int nStudents = sc.nextInt();
+        for (int i = 0; i < nStudents; i++){
+            int id = sc.nextInt();
 
-
-        list.add(order1);
-        list.add(order2);
-        list.add(order3);
-        list.add(order4);
-
-        Collections.sort(list);
-
-        for (Order o : list){
-            o.getPaymentMethod().printReceipt(o.getTotal());
+            set.add(new Course(id));
         }
 
+        System.out.print("How many students for course B? ");
+        int nStudents2 = sc.nextInt();
+        for (int i = 0; i < nStudents2; i++){
+            int id = sc.nextInt();
 
+            set.add(new Course(id));
+        }
+
+        System.out.print("How many students for course C? ");
+        int nStudents3 = sc.nextInt();
+        for (int i = 0; i < nStudents3; i++){
+            int id = sc.nextInt();
+
+            set.add(new Course(id));
+        }
+
+        System.out.println("Total students: " + set.size());
         sc.close();
     }
 }
