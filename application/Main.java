cd52b35544217db76a1dd72b26ef5e2921877a83
Rafael Rocha <rafaelrocha250304@gmail.com>
2026-05-19 13:06:52 -0300

    treinando streams

--- application/Main.java
+++ application/Main.java
@@ -1,7 +1,11 @@
 package application;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -10,20 +14,43 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        List<Integer> list = Arrays.asList(3, 4, 5, 6, 10);
+        String path = "/Users/rafaelrocha/Documents/PROJETOS/testes-java/exercicio 1/application/in.csv";
 
-        Stream<Integer> st1 = list.stream().map(x -> x * 10);
-        System.out.println(Arrays.toString(st1.toArray()));
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            List<Employee> list = new ArrayList<>();
 
-        Stream<String> st2 = Stream.of("Maria", "Rafael", "Roberto", "Andrea").map(String::toUpperCase);
-        System.out.println(Arrays.toString(st2.toArray()));
+            String line = br.readLine();
+            while(line != null){
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
 
-        Stream<Integer> st3 = Stream.iterate(2 , x -> x * 2);
-        System.out.println(Arrays.toString(st3.limit(100).toArray()));
 
-        Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0] + p[1]}).map(p -> p[0]);
-        System.out.println(Arrays.toString(st4.limit(20).toArray()));
 
-       sc.close();
+            System.out.print("Enter salary: ");
+            double salary = sc.nextDouble();
+
+            List<String> emailsUp = list.stream()
+                    .filter(p -> p.getSalary() > salary)
+                    .map(p -> p.getEmail())
+                    .collect(Collectors.toList());
+
+            System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary));
+            emailsUp.forEach(System.out::println);
+
+            double soma = list.stream()
+                    .filter(p -> p.getName().charAt(0) == 'M')
+                    .map(p -> p.getSalary())
+                    .reduce(0.0, Double::sum);
+
+
+            System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", soma));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        sc.close();
     }
 }
